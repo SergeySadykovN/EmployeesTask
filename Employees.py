ab4c787578e0a3a676de85d5c2413690bffea854
SergeySadykovN <snsadykov@ya.ru>
2024-04-12 12:23:12 +0300

    v.03 add reg.ex for sort_type by lower and target values in table

--- Employees.py
+++ Employees.py
@@ -1,9 +1,17 @@
 import json
+import re
 
 
 def employees_rewrite(sort_type):
     with open('employees.json', 'r') as file:
         json_data = json.load(file)  # -> dict
+
+        # приводим входное слово к  нижнему регистру и прописываем рег.выраж для того же в  искомом файле
+        sort_type = sort_type.lower()
+        pattern = 'n'
+        if sort_type == 'lastname' or sort_type == 'firstname':
+            sort_type = re.sub(pattern, 'N', sort_type.lower())
+
         sort_data = sorted(json_data['employees'], key=lambda x: x[sort_type])
         if type(json_data['employees'][0][sort_type]) is int:
             sort_data = sorted(json_data['employees'], key=lambda x: x[sort_type], reverse=True)
@@ -15,6 +23,7 @@ def employees_rewrite(sort_type):
 
 
 employees_rewrite('salary')
-employees_rewrite('firstName')
-employees_rewrite('lastName')
+employees_rewrite('firstname')
+employees_rewrite('lastname')
+employees_rewrite('LastName')
 employees_rewrite('any')
